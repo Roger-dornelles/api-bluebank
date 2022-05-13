4d8d667c38254ef7fa106fd87d771a198713fdca
Roger <rsd.santo@outlook.com>
2022-05-13 14:11:22 -0300

    remoção de visualização de error no controller transference

--- src/controllers/TransferenceController.js
+++ src/controllers/TransferenceController.js
@@ -127,7 +127,7 @@ module.exports = {
 
                 }else{
                     res.status(200);
-                    res.json(`${value}, ${account}, ${bank}, ${type_destiny_account}, ${agency}, ${favored_name}, ${document}`);
+                    res.json({error:'Preencha todos os campos.'});
                 };
 
             }else{
